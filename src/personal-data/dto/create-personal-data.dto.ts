28a8be427c7d7bceabd22b87e84c613345a93017
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-08 18:58:51 +0100

    added personal data dto

---
+++ src/personal-data/dto/create-personal-data.dto.ts
@@ -0,0 +1,33 @@
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+
+/**
+ * This DTO contains information about userPersonalData entity properties types
+ */
+export class CreatePersonalDataDto {
+  @IsString({
+    message: 'User name must be string type',
+  })
+  @IsNotEmpty({
+    message: 'User name cannot be empty string',
+  })
+  @Length(0, 70)
+  name: string;
+
+  @IsString({
+    message: 'User surname must be string type',
+  })
+  @IsNotEmpty({
+    message: 'User surname cannot be empty string',
+  })
+  @Length(0, 250)
+  surname: string;
+
+  @IsString({
+    message: 'Apartment number must be string type',
+  })
+  @IsNotEmpty({
+    message: 'Apartment number cannot be empty string',
+  })
+  @Length(0, 13)
+  phone_number: string;
+}
