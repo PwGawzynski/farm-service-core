03d5c8242553695a87885c9889b64f3459835072
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-08 15:38:30 +0100

    created auth controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UserDataDto } from './dto/user-data.dto';
+import { Request } from 'express';
+import { JwtRefreshAuthGuard } from './jwt-refresh-auth.guard';
+import { Public } from '../../decorators/auth.decorators';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  /**
+   * auth/login req, serves login asks
+   * @param userPayload data transferred as JSON in req body
+   * @return data object with access token and refresh token
+   */
+  @Post('login')
+  @Public()
+  login(@Body() userPayload: UserDataDto) {
+    return this.authService.login(userPayload);
+  }
+
+  /**
+   * auth/refresh this method serves refresh token ask
+   * @param req express type request object taken by decorator
+   * @return data object with access token and refresh token
+   */
+  @UseGuards(JwtRefreshAuthGuard)
+  @Post('refresh')
+  refreshToken(@Req() req: Request) {
+    return this.authService.refreshToken(req);
+  }
+}
