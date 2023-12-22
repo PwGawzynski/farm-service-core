71b3418d4e38bc05ecc763bea8d5b0ec65a69236
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-22 22:26:12 +0100

    changed

--- src/auth/dto/user-data.dto.ts
+++ src/auth/dto/user-data.dto.ts
@@ -1,6 +1,41 @@
-import { CreateUserDto } from '../../user/dto/create-user.dto';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+  Length,
+} from 'class-validator';
 
 /**
  * Data transfer object, used to specify expected in request data object
  */
-export class UserDataDto extends CreateUserDto {}
+export class UserDataDto {
+  @IsString({
+    message: 'Login must be string type',
+  })
+  @IsNotEmpty({
+    message: 'Login can not be empty string',
+  })
+  @Length(1, 350, {
+    message: 'Length must be in 1 to 350 characters',
+  })
+  @IsEmail()
+  email: string;
+
+  @IsString({
+    message: 'Password must be string type',
+  })
+  @IsNotEmpty({
+    message: 'Password can not be empty string',
+  })
+  @Length(1, 200, {
+    message: 'Length must be in 1 to 200 characters',
+  })
+  @IsStrongPassword({
+    minLength: 8,
+    minNumbers: 1,
+    minUppercase: 1,
+    minSymbols: 1,
+  })
+  password: string;
+}
