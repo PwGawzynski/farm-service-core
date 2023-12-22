7aa010892aa94c8bb8cc6e4b43411dca9403639c
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-22 22:25:27 +0100

    added address

---
+++ src/address/dto/create-address.dto.ts
@@ -0,0 +1,96 @@
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Length,
+  Matches,
+} from 'class-validator';
+import AddressConstants from '../../../FarmServiceApiTypes/Address/Constants';
+
+/**
+ * This DTO represent all address entities in application
+ */
+export class CreateAddressDto {
+  @IsString({
+    message: 'City must be string type',
+  })
+  @IsNotEmpty({
+    message: 'City cannot be empty string',
+  })
+  @Length(AddressConstants.CITY_MIN_LEN, AddressConstants.CITY_MAX_LEN)
+  city: string;
+
+  @IsString({
+    message: 'County must be string type',
+  })
+  @IsNotEmpty({
+    message: 'County cannot be empty string',
+  })
+  @Length(AddressConstants.COUNTY_MIN_LEN, AddressConstants.COUNTY_MAX_LEN)
+  county: string;
+
+  @IsString({
+    message: 'Voivodeship must be string type',
+  })
+  @IsNotEmpty({
+    message: 'Voivodeship cannot be empty string',
+  })
+  @Length(
+    AddressConstants.VOIVODESHIP_MIN_LEN,
+    AddressConstants.VOIVODESHIP_MAX_LEN,
+  )
+  @IsOptional()
+  voivodeship?: string;
+
+  @IsString({
+    message: 'Street must be string type',
+  })
+  @IsNotEmpty({
+    message: 'Street cannot be empty string',
+  })
+  @Length(AddressConstants.STREET_MIN_LEN, AddressConstants.STREET_MAX_LEN)
+  @IsOptional()
+  street?: string;
+
+  @IsString({
+    message: 'House number must be string type',
+  })
+  @IsNotEmpty({
+    message: 'House number cannot be empty string',
+  })
+  @Length(AddressConstants.HOUSE_NR_MIN_LEN, AddressConstants.HOUSE_NR_MAX_LEN)
+  houseNumber: string;
+
+  @IsString({
+    message: 'Apartment number must be string type',
+  })
+  @IsNotEmpty({
+    message: 'Apartment number cannot be empty string',
+  })
+  @Length(
+    AddressConstants.APARTMENT_NR_MIN_LEN,
+    AddressConstants.APARTMENT_NR_MAX_LEN,
+  )
+  @IsOptional()
+  apartmentNumber?: string;
+
+  @IsString({
+    message: 'Postal code must be string type',
+  })
+  @IsNotEmpty({
+    message: 'Postal code cannot be empty string',
+  })
+  @Matches('^[0-9]{2}-[0-9]{3}$')
+  @Length(AddressConstants.POSTAL_CODE_LEN, AddressConstants.POSTAL_CODE_LEN)
+  postalCode: string;
+
+  public *[Symbol.iterator]() {
+    yield this.city;
+    yield this.county;
+    yield this.voivodeship;
+    yield this.postalCode;
+    yield this.street;
+    yield this.houseNumber;
+    yield this.apartmentNumber;
+  }
+}
---
+++ src/address/entities/address.entity.ts
@@ -0,0 +1,79 @@
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { User } from '../../user/entities/user.entity';
+
+/**
+ * This column represents all address in application, is used to store both user and company addresses
+ */
+@Entity()
+export class Address extends BaseEntity {
+  constructor(props?: {
+    city: string;
+    county: string;
+    postalCode: string;
+    houseNumber: string;
+    apartmentNumber?: string | undefined;
+    user?: Promise<User>;
+    voivodeship?: string;
+    street?: string;
+  }) {
+    super();
+    if (props) Object.assign(this, props);
+  }
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    nullable: false,
+    length: 70,
+  })
+  city: string;
+
+  @Column({
+    length: 50,
+    nullable: false,
+  })
+  county: string;
+
+  @Column({
+    length: 50,
+    nullable: true,
+  })
+  voivodeship?: string;
+
+  @Column({
+    length: 6,
+    nullable: false,
+  })
+  postalCode: string;
+
+  @Column({
+    length: 100,
+    nullable: true,
+  })
+  street?: string;
+
+  @Column({
+    length: 20,
+    nullable: false,
+  })
+  houseNumber: string;
+
+  @Column({
+    length: 20,
+    nullable: true,
+  })
+  apartmentNumber?: string | undefined;
+
+  @OneToOne(() => User, (user) => user.address, { nullable: true })
+  @JoinColumn()
+  @Index('UNIQ_USER', { unique: true })
+  user?: Promise<User | null>;
+}
