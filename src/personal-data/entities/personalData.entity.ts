c6e151662432b5feffdbe49b09ca30e281d2a0dc
pwgawzynski <pgawzynski.backup@gmail.com>
2023-12-22 22:23:37 +0100

    added constructor

--- src/personal-data/entities/personalData.entity.ts
+++ src/personal-data/entities/personalData.entity.ts
@@ -7,9 +7,19 @@ import {
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { User } from '../../user/entities/user.entity';
+import { ConflictException } from '@nestjs/common';
 
 @Entity()
 export class PersonalData extends BaseEntity {
+  constructor(props?: {
+    name: string;
+    surname: string;
+    phoneNumber: string;
+    user?: Promise<User>;
+  }) {
+    super();
+    if (props) Object.assign(this, props);
+  }
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
@@ -34,4 +44,22 @@ export class PersonalData extends BaseEntity {
   @OneToOne(() => User, (user) => user.personalData, { nullable: true })
   @JoinColumn()
   user: Promise<User>;
+
+  /**
+   * Checks if entity already exist in db
+   * @param key one of PersonalData properties [keyof User]
+   * @param conflictMsg message sent when conflict
+   * @throws ConflictException when exist
+   */
+  async _shouldNotExist<T extends keyof PersonalData>(
+    key: T,
+    conflictMsg: string,
+  ) {
+    const exist = await PersonalData.findOne({
+      where: {
+        [key]: this[key],
+      },
+    });
+    if (exist) throw new ConflictException(conflictMsg);
+  }
 }
