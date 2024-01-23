0fc5d6354efda0b064798fcc64765c3b1c2c1a28
pwgawzynski <pgawzynski.backup@gmail.com>
2024-01-23 08:57:59 +0100

    added invisible relation to Company

--- src/address/entities/address.entity.ts
+++ src/address/entities/address.entity.ts
@@ -2,12 +2,11 @@ import {
   BaseEntity,
   Column,
   Entity,
-  Index,
-  JoinColumn,
   OneToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { User } from '../../user/entities/user.entity';
+import { Company } from '../../company/entities/company.entity';
 
 /**
  * This column represents all address in application, is used to store both user and company addresses
@@ -73,7 +72,8 @@ export class Address extends BaseEntity {
   apartmentNumber?: string | undefined;
 
   @OneToOne(() => User, (user) => user.address, { nullable: true })
-  @JoinColumn()
-  @Index('UNIQ_USER', { unique: true })
   user?: Promise<User | null>;
+
+  @OneToOne(() => Company, (company) => company.address, { nullable: true })
+  company?: Promise<Company | null>;
 }
