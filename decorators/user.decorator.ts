60923eab87719a5e5492ef4cea112319bad12947
pwgawzynski <pgawzynski.backup@gmail.com>
2024-01-23 20:11:57 +0100

    added decorator to fastify a getting owner's company

--- decorators/user.decorator.ts
+++ decorators/user.decorator.ts
@@ -1,10 +1,13 @@
 import {
+  ConflictException,
   createParamDecorator,
   ExecutionContext,
   HttpException,
   HttpStatus,
+  InternalServerErrorException,
 } from '@nestjs/common';
 import { printWarnToConsole } from '../Helpers/printWarnToConsole';
+import { User } from '../src/user/entities/user.entity';
 
 export function throwError(msg: string, warnMsg: string, location: string) {
   printWarnToConsole(warnMsg, location);
@@ -18,5 +21,18 @@ export const GetUser = createParamDecorator(
   async (data: unknown, ctx: ExecutionContext) => {
     console.log(ctx.switchToHttp().getRequest(), 'user');
     return ctx.switchToHttp().getRequest().user;
+  },
+);
+
+export const GetOwnedCompany = createParamDecorator(
+  async (data: unknown, ctx: ExecutionContext) => {
+    const user = ctx.switchToHttp().getRequest().user;
+    if (!(user instanceof User)) {
+      printWarnToConsole('REQ.USER IS UNDEFENDED', 'GET_OWNED_COMPANY');
+      throw new InternalServerErrorException(undefined, 'Something went wrong');
+    }
+    const company = await user.company;
+    if (!company) throw new ConflictException("Causer don't have company");
+    return company;
   },
 );
