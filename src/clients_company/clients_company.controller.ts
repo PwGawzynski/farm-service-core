f8687bea2cc12d2fdaa3ab06d493a7f4285bc83d
pwgawzynski <pgawzynski.backup@gmail.com>
2024-02-12 08:33:58 +0100

    created - endpoint to assign company data and update company data

--- src/clients_company/clients_company.controller.ts
+++ src/clients_company/clients_company.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Put } from '@nestjs/common';
 import { ClientsCompanyService } from './clients_company.service';
+import { Owner } from '../../decorators/auth.decorators';
+import { AssignCompanyDataToClientDto } from './dto/assign_company.dto';
+import { UpdateClientsCompanyDto } from './dto/update-clients_company.dto';
 
 @Controller('clients-company')
 export class ClientsCompanyController {
   constructor(private readonly clientsCompanyService: ClientsCompanyService) {}
+
+  @Post()
+  @Owner()
+  assignCompanyToClient(
+    @Body() assignCompanyDto: AssignCompanyDataToClientDto,
+  ) {
+    return this.clientsCompanyService.assignCompanyToClient(assignCompanyDto);
+  }
+
+  @Put()
+  @Owner()
+  update(@Body() updateData: UpdateClientsCompanyDto) {
+    return this.clientsCompanyService.update(updateData);
+  }
 }
