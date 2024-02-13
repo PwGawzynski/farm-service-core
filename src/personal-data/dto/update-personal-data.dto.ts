49686fdeb9832878924e7dd311b7c04229189f68
pwgawzynski <pgawzynski.backup@gmail.com>
2024-02-13 22:20:29 +0100

    created update dtos

---
+++ src/personal-data/dto/update-personal-data.dto.ts
@@ -0,0 +1,6 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreatePersonalDataDto } from './create-personal-data.dto';
+
+export class UpdatePersonalDataDto extends PartialType(CreatePersonalDataDto) {
+  id: string;
+}
