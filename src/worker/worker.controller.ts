002302f8567e3cf2e1bd9d1f40afbfe6a4f0b9e0
pwgawzynski <pgawzynski.backup@gmail.com>
2024-02-26 20:33:59 +0100

    added endpoints to get worker  info

--- src/worker/worker.controller.ts
+++ src/worker/worker.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Sse } from '@nestjs/common';
 import { WorkerService } from './worker.service';
-import { Owner } from '../../decorators/auth.decorators';
+import { Owner, Worker as WorkerRole } from '../../decorators/auth.decorators';
 import { CreateWorkerDto } from './dto/create-worker.dto';
-import { GetOwnedCompany } from '../../decorators/user.decorator';
+import {
+  GetOwnedCompany,
+  GetUser,
+  GetWorker,
+} from '../../decorators/user.decorator';
 import { Company } from '../company/entities/company.entity';
-
+import { Worker } from './entities/worker.entity';
+import { User } from '../user/entities/user.entity';
 @Controller('worker')
 export class WorkerController {
   constructor(private readonly workerService: WorkerService) {}
@@ -16,5 +21,16 @@ export class WorkerController {
     @GetOwnedCompany() company: Company,
   ) {
     return this.workerService.assign(createWorkerDto, company);
+  }
+
+  @Get()
+  @WorkerRole()
+  async getInfo(@GetWorker() worker: Worker) {
+    return this.workerService.getInfo(worker);
+  }
+
+  @Sse('sse/status')
+  status(@GetUser() user: User) {
+    return this.workerService.status(user);
   }
 }
