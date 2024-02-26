c3f0c67b8c110f1fc28bafc194e9c42d6f925f99
pwgawzynski <pgawzynski.backup@gmail.com>
2024-02-26 20:34:44 +0100

    created sse assign worker service

--- src/worker/worker.service.ts
+++ src/worker/worker.service.ts
@@ -9,9 +9,25 @@ import {
 import { WorkerResponseDto } from './dto/response/worker-response.dto';
 import { AddressResponseDto } from '../address/dto/response/address.response.dto';
 import { PersonalDataResponseDto } from '../personal-data/dto/response/personalData-response.dto';
+import { concatMap, filter, interval, take } from 'rxjs';
+import { User } from '../user/entities/user.entity';
 
 @Injectable()
 export class WorkerService {
+  async createWorkerResponseDto(worker: Worker) {
+    const user = await worker.user;
+    const account = await user.account;
+    const address = await user.address;
+    const personalData = await user.personalData;
+    return new WorkerResponseDto({
+      status: worker.status,
+      position: worker.position,
+      email: account.email,
+      address: new AddressResponseDto(address),
+      personalData: new PersonalDataResponseDto(personalData),
+    });
+  }
+
   async assign(createWorkerDto: CreateWorkerDto, company: Company) {
     const user = createWorkerDto.user;
     const worker = new Worker({
@@ -22,12 +38,37 @@ export class WorkerService {
     worker.save();
     return {
       code: ResponseCode.ProcessedCorrect,
-      payload: new WorkerResponseDto({
-        ...worker,
-        email: (await user.account).email,
-        address: new AddressResponseDto(await user.address),
-        personalData: new PersonalDataResponseDto(await user.personalData),
-      }),
+      payload: await this.createWorkerResponseDto(worker),
     } as ResponseObject<WorkerResponseDto>;
+  }
+
+  async getInfo(worker: Worker) {
+    return {
+      code: ResponseCode.ProcessedCorrect,
+      payload: await this.createWorkerResponseDto(worker),
+    } as ResponseObject<WorkerResponseDto>;
+  }
+
+  async status(user: User) {
+    return interval(2000).pipe(
+      concatMap(async () => {
+        const worker = await Worker.findOne({
+          where: { user: { id: user.id } },
+        });
+        if (worker) {
+          return JSON.stringify({
+            code: ResponseCode.ProcessedCorrect,
+            payload: await this.createWorkerResponseDto(worker),
+          } as ResponseObject<WorkerResponseDto>);
+        }
+        return JSON.stringify({
+          code: ResponseCode.ErrorOccurred,
+        } as ResponseObject<WorkerResponseDto>);
+      }),
+      filter(
+        (result) => JSON.parse(result).code === ResponseCode.ProcessedCorrect,
+      ),
+      take(2),
+    );
   }
 }
