89b2bb2e986945ded5160fbb934a9287610fa2d0
pwgawzynski <pgawzynski.backup@gmail.com>
2024-03-17 19:09:13 +0100

    added field module

---
+++ src/field/field.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { FieldService } from './field.service';
+import { FieldController } from './field.controller';
+import { HttpModule } from '@nestjs/axios';
+
+@Module({
+  imports: [
+    HttpModule.registerAsync({
+      useFactory: () => ({
+        timeout: 5000,
+        maxRedirects: 5,
+      }),
+    }),
+  ],
+  controllers: [FieldController],
+  providers: [FieldService],
+})
+export class FieldModule {}
