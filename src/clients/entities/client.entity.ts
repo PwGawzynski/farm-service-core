d1eb6bcad8937f999eba6d95a173678025b080fd
pwgawzynski <pgawzynski.backup@gmail.com>
2024-03-29 19:38:32 +0100

    added relation to order

--- src/clients/entities/client.entity.ts
+++ src/clients/entities/client.entity.ts
@@ -3,12 +3,14 @@ import {
   Entity,
   JoinColumn,
   ManyToOne,
+  OneToMany,
   OneToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { User } from '../../user/entities/user.entity';
 import { ClientsCompany } from '../../clients_company/entities/clients_company.entity';
 import { Company } from '../../company/entities/company.entity';
+import { Order } from '../../order/entities/order.entity';
 
 @Entity()
 export class Client extends BaseEntity {
@@ -31,4 +33,7 @@ export class Client extends BaseEntity {
 
   @ManyToOne(() => Company, (company) => company.clients, { nullable: false })
   isClientOf: Promise<Company | null>;
+
+  @OneToMany(() => Order, (order) => order.client, { nullable: true })
+  orders: Promise<Order[] | null>;
 }
