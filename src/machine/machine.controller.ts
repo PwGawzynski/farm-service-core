c262a25f0618215cbefeffbabe83af7247b5e181
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-08 18:52:46 +0200

    added endpoints - post to create - get all - put to update - post to safely-delete

--- src/machine/machine.controller.ts
+++ src/machine/machine.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Put } from '@nestjs/common';
 import { MachineService } from './machine.service';
 import { Owner } from '../../decorators/auth.decorators';
 import { CreateMachineDto } from './dto/create-machine.dto';
 import { GetOwnedCompany } from '../../decorators/user.decorator';
 import { Company } from '../company/entities/company.entity';
+import { UpdateMachineDto } from './dto/update-machine.dto';
 
 @Controller('machine')
 export class MachineController {
@@ -16,5 +17,29 @@ export class MachineController {
     @GetOwnedCompany() company: Company,
   ) {
     return this.machineService.create(createMachineDto, company);
+  }
+
+  @Get('all')
+  @Owner()
+  async getAll(@GetOwnedCompany() company: Company) {
+    return this.machineService.getAll(company);
+  }
+
+  @Put()
+  @Owner()
+  async update(
+    @Body() updateData: UpdateMachineDto,
+    @GetOwnedCompany() company: Company,
+  ) {
+    return this.machineService.update(updateData, company);
+  }
+
+  @Post('safely-delete')
+  @Owner()
+  async safelyDelete(
+    @Body() deleteData: UpdateMachineDto,
+    @GetOwnedCompany() company: Company,
+  ) {
+    return this.machineService.safelyDelete(deleteData, company);
   }
 }
