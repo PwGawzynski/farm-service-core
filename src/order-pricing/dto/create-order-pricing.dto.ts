c493f51f8c8121ff3261c991247680953ac774cf
pwgawzynski <pgawzynski.backup@gmail.com>
2024-05-10 20:30:17 +0200

    added more validation

--- src/order-pricing/dto/create-order-pricing.dto.ts
+++ src/order-pricing/dto/create-order-pricing.dto.ts
@@ -1,15 +1,20 @@
-import { IsEnum, IsNumber } from 'class-validator';
+import { IsNumber, Max, Min } from 'class-validator';
 import { TaskType } from 'FarmServiceApiTypes/Task/Enums';
 import { FindOrReject } from '../../../ClassValidatorCustomDecorators/FindOrReject.decorator';
 import { Order } from '../../order/entities/order.entity';
+import { Transform } from 'class-transformer';
+import { OrderPricingConstants } from '../../../FarmServiceApiTypes/OrderPricing/Constants';
 
 export class CreateOrderPricingDto {
   @FindOrReject(Order, { message: 'Order not found' })
   order: Order;
-  @IsEnum(TaskType)
-  taskType: TaskType;
-  @IsNumber()
-  price: number;
-  @IsNumber()
+
+  @IsNumber(undefined, { each: true })
+  @Transform(({ value }) => new Map(value))
+  prices: Map<keyof typeof TaskType, number>;
+
+  @Transform(({ value }) => Number(value))
+  @Min(OrderPricingConstants.MIN_TAX)
+  @Max(OrderPricingConstants.MAX_TAX)
   tax: number;
 }
