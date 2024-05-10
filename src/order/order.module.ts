5537bde74c27b358ce3cc7bb808782fe7845cdcb
pwgawzynski <pgawzynski.backup@gmail.com>
2024-05-10 18:26:15 +0200

    imported order pricing service

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { OrderPricingService } from '../order-pricing/order-pricing.service';
 
 @Module({
   controllers: [OrderController],
-  providers: [OrderService],
+  providers: [OrderService, OrderPricingService],
 })
 export class OrderModule {}
