22ca8d96f67859a497d01153417db67de21e3acb
pwgawzynski <pgawzynski.backup@gmail.com>
2024-05-21 08:02:18 +0200

    added to cnf

--- config/configuration.ts
+++ config/configuration.ts
@@ -6,4 +6,6 @@ export default () => ({
   maxRegisteredDevicesCount: process.env.MaxRegisteredDevicesCount,
   refreshSign: process.env.SecretSign,
   secretSign: process.env.RefreshSign,
+  iosClientId: process.env.IOS_CLINET_ID,
+  androidClientId: process.env.ANDROID_CLIENT_ID,
 });
