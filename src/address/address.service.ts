6b83f99868af6f061a9ef79ccbaf8dff90a2a2f5
pwgawzynski <pgawzynski.backup@gmail.com>
2024-05-27 22:46:03 +0200

    changed to new error response structure

--- src/address/address.service.ts
+++ src/address/address.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { UpdateAddressDto } from './dto/update-address.dto';
 import { Address } from './entities/address.entity';
 import {
@@ -7,13 +7,19 @@ import {
 } from '../../FarmServiceApiTypes/Respnse/responseGeneric';
 import { AddressResponseDto } from './dto/response/address.response.dto';
 import { Equal } from 'typeorm';
+import { ErrorPayloadObject } from '../../FarmServiceApiTypes/Respnse/errorPayloadObject';
+import { InvalidRequestCodes } from '../../FarmServiceApiTypes/InvalidRequestCodes';
 
 @Injectable()
 export class AddressService {
   async update(updateData: UpdateAddressDto) {
     const { id, ...address } = updateData;
     const oldAddress = await Address.findOne({ where: { id: Equal(id) } });
-    if (!oldAddress) throw new BadRequestException('Address not exist');
+    if (!oldAddress)
+      throw new NotFoundException({
+        code: InvalidRequestCodes.address_notFound,
+        message: 'Address not found',
+      } as ErrorPayloadObject);
     Address.createQueryBuilder()
       .update(oldAddress)
       .set(address)
