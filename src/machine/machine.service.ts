5b75b2a48699030a9f61026b63707dffdbc2bb50
pwgawzynski <pgawzynski.backup@gmail.com>
2024-05-27 23:00:36 +0200

    changed to new error response structure

--- src/machine/machine.service.ts
+++ src/machine/machine.service.ts
@@ -1,4 +1,8 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+} from '@nestjs/common';
 import { Company } from '../company/entities/company.entity';
 import { CreateMachineDto } from './dto/create-machine.dto';
 import { Machine } from './entities/machine.entity';
@@ -9,9 +13,18 @@ import {
 import { MachineResponseDto } from './dto/response/machine.response.dto';
 import { UpdateMachineDto } from './dto/update-machine.dto';
 import { v4 as uuid } from 'uuid';
+import { ErrorPayloadObject } from '../../FarmServiceApiTypes/Respnse/errorPayloadObject';
+import { InvalidRequestCodes } from '../../FarmServiceApiTypes/InvalidRequestCodes';
 
 @Injectable()
 export class MachineService {
+  //--------------------------------HELPER METHODS--------------------------------//
+
+  /**
+   * Prepare response dto
+   * @param machine
+   * @returns {Promise<MachineResponseDto>}
+   */
   async prepareResponseDto(machine: Machine) {
     return new MachineResponseDto({
       id: machine.id,
@@ -20,8 +33,44 @@ export class MachineService {
     });
   }
 
+  //--------------------------------VALIDATORS--------------------------------//
+
+  /**
+   * Check if license plate is unique
+   * @param licensePlate
+   * @returns {Promise<void>}
+   * @throws {ConflictException} if license plate is not unique
+   * @private
+   */
+  private async _uniqPlate(licensePlate: string) {
+    if (await Machine.findOne({ where: { licensePlate } }))
+      throw new ConflictException({
+        message: 'Machine with this license plate already exists',
+        code: InvalidRequestCodes.machine_licencePlateTaken,
+      } as ErrorPayloadObject);
+  }
+
+  /**
+   * Check if the machine belongs to company
+   * @param company
+   * @param machine
+   * @returns {Promise<void>}
+   * @throws {BadRequestException} if machine does not belong to company
+   * @private
+   */
+  private async _checkIfBelongs(company: Company, machine: Machine) {
+    if (company.id !== (await machine.company)?.id)
+      throw new BadRequestException({
+        message: 'Machine does not belong to company',
+        code: InvalidRequestCodes.machine_doesNotBelongToCompany,
+      } as ErrorPayloadObject);
+  }
+
+  //--------------------------------CRUD--------------------------------//
+
   async create(createMachineDto: CreateMachineDto, company: Company) {
     const machine = new Machine(createMachineDto);
+    await this._uniqPlate(machine.licensePlate);
     machine.company = Promise.resolve(company);
     machine.id = uuid();
     machine.save();
@@ -31,6 +80,10 @@ export class MachineService {
     } as ResponseObject<MachineResponseDto>;
   }
 
+  /**
+   * Returns all undeleted machines, belonging to company
+   * @param company
+   */
   async getAll(company: Company) {
     const machines = (await company.machines)?.filter((m) => !m.deletedAt);
     if (!machines || machines.length === 0)
@@ -45,10 +98,8 @@ export class MachineService {
   }
 
   async update(updateData: UpdateMachineDto, company: Company) {
-    const machine = (await company.machines)?.find(
-      (m) => m.id === updateData.machine.id && !m.deletedAt,
-    );
-    if (!machine) throw new ConflictException('Machine not found');
+    await this._checkIfBelongs(company, updateData.machine);
+    const machine = updateData.machine;
     machine.name = updateData.name || machine.name;
     machine.licensePlate = updateData.licensePlate || machine.licensePlate;
     machine.save();
@@ -58,11 +109,14 @@ export class MachineService {
     } as ResponseObject<MachineResponseDto>;
   }
 
+  /**
+   * Safely delete machine -- add deletedAt date
+   * @param deleteData
+   * @param company
+   */
   async safelyDelete(deleteData: UpdateMachineDto, company: Company) {
-    const machine = (await company.machines)?.find(
-      (m) => m.id === deleteData.machine.id && !m.deletedAt,
-    );
-    if (!machine) throw new ConflictException('Machine not found');
+    await this._checkIfBelongs(company, deleteData.machine);
+    const machine = deleteData.machine;
     machine.deletedAt = new Date();
     machine.save();
     return {
